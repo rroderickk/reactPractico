d5a9d88eec7d6b948053dbbb92c5ded395aa8578
RodrigoMoreno <rroderickk@gmail.com>
2022-02-01 18:48:33 -0500

    desplega

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
-        options: { presets: ['@babel/env','@babel/preset-react'] },
-        extensions: ['.js', '.jsx' ],
+        // options: { presets: ['@babel/env','@babel/preset-react'] },
       },
       {
         test: /\.(png|jpg|svg|jpeg|web|svg)$/,
